629b78db0305c7694c0ffde3ccc17eecced1f161
Jamile Celento <jesperanca@stone.com.br>
2017-10-29 19:27:25 -0200

    Bower removed and npm added to the project

---
+++ EletronicPartsCatalog/FrontEnd/Homepage/Components/Demo/Demo.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './Demo.scss';
+
+class Demo extends React.Component<any, any>{
+	
+	public render(){
+	return ( 
+	<div className="Demo">
+	<p>ASP.NET + React, from Jamile </p>
+	</div>
+	)
+  }
+}
+
+export default Demo;
