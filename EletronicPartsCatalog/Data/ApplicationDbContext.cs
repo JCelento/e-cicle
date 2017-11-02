32d7bfbca6c8ef25c8d2943ba871207908e55a9d
Jamile Celento <jesperanca@stone.com.br>
2017-11-02 19:00:04 -0200

    Adding the entity "project" to database

--- EletronicPartsCatalog/Data/ApplicationDbContext.cs
+++ EletronicPartsCatalog/Data/ApplicationDbContext.cs
@@ -9,6 +9,8 @@ namespace EletronicPartsCatalog.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Project> Projects { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
@@ -20,6 +22,8 @@ namespace EletronicPartsCatalog.Data
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.Entity<Project>().HasKey(k => k.Id);
         }
     }
 }
--- EletronicPartsCatalog/Data/Migrations/20171102205102_AddedProjectEntity.Designer.cs
+++ EletronicPartsCatalog/Data/Migrations/20171102205102_AddedProjectEntity.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace EletronicPartsCatalog.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171102205102_AddedProjectEntity")]
+    partial class AddedProjectEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
