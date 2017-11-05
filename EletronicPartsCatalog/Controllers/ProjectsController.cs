93f427dfaa6b5a133cf8be65e2a16340d2ecf30d
Jamile Celento <jesperanca@stone.com.br>
2017-11-05 17:34:48 -0200

    Adding ProjectsController

---
+++ EletronicPartsCatalog/Controllers/ProjectsController.cs
@@ -0,0 +1,24 @@
+﻿using EletronicPartsCatalog.Contracts.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EletronicPartsCatalog.Web.Controllers
+{
+    [Authorize]
+    public class ProjectsController : Controller
+    {
+        private IProjectsService _projectsService;
+
+        public ProjectsController(IProjectsService projectsService) {
+            _projectsService = projectsService;
+        }
+        [HttpGet]
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
