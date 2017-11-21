26e8a56ccc74117b8c52752cd09c2ddcbe2140dd
Jamile Celento <jesperanca@stone.com.br>
2017-11-21 21:25:32 -0200

    Adding button testing and changing button label

--- EletronicPartsCatalog/FrontEnd/Projects/Components/EmptyListWarning/EmptyListWarning.tsx
+++ EletronicPartsCatalog/FrontEnd/Projects/Components/EmptyListWarning/EmptyListWarning.tsx
@@ -4,21 +4,21 @@ import './EmptyListWarning.scss';
 import Button from '../../../Shared/Components/Button';
 
 class EmptyListWarning extends React.Component<any, any>{
-	
-	private paths = {
-	createProjectUrl: '/Projects/Add',
-	createProjectLogo: ''
-	};
+    
+    private paths = {
+    createProjectUrl: '/Projects/Add',
+    createProjectLogo: ''
+    };
 
-	public render(){
-	return(
-		<section className='EmptyListWarning'>
-		<img className='center-block' src='/images/List.png' />
-		<section className='EmptyListWarning-textContainer'>
-		<p className='text-center'>It looks like you don't have any projects created.</p>
-		<p className='text-center'>
-	 <Button link={true} href={this.paths.createProjectUrl} extraClassNames={['btn-success']}>
-                            Create first project
+    public render(){
+    return(
+        <section className='EmptyListWarning'>
+        <img className='center-block' src='/images/List.png' />
+        <section className='EmptyListWarning-textContainer'>
+        <p className='text-center'>Ainda não temos nenhum projeto criado :(</p>
+        <p className='text-center'>
+     <Button link={true} href={this.paths.createProjectUrl} extraClassNames={['btn-success']}>
+                            Criar um projeto
                         </Button>
                     </p>
                 </section>
